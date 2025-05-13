3487d67fc6751a65c1dcaa60f3f7c36825d4ce59
Diego Michel Prestes <diegoprestes99@hotmail.com>
2025-05-12 23:09:29 -0300

    [fix] bug

--- Classe1.java
+++
@@ -1,5 +0,0 @@
-public class Classe1 {
-	public static void main(String[] args) {
-		System.out.println("Modificado no branch new_freature");
-	};
-};
--- Classe2.java
+++
@@ -1,5 +0,0 @@
-public class Classe2 {
-	public static void main(String[] args) {
-		System.out.println("Classe2 do another_feature");
-	};
-};
